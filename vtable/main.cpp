30bab44f8647d3d564a3127c2d0072a5ad7d2a52
Olga Sopilnyak <o.sopilniak@gmail.com>
2018-04-27 03:15:08 +0300

    Add second version of vtable

--- vtable/main.cpp
+++ vtable/main.cpp
@@ -1,98 +1,124 @@
 #include <iostream>
 #include <map>
-#include <string>
 #include <memory>
-#include <vector>
+#include <unordered_map>
 
 class Vtable {
 public:
     Vtable() = default;
 
-    explicit Vtable(std::map<std::string, void*> map) {
-        const auto &newMap(map);
+    explicit Vtable(std::unordered_map<std::string, void*>& table) {
+        std::unordered_map<std::string, void*> newMap(table);
         buffer = newMap;
     }
 
-    void* getFuncPtr(std::string funcName) {
-        if (buffer.find(funcName) == buffer.end()) {
-            throw std::exception();
+    void addNew(std::unordered_map<std::string, void*>& table) {
+        for (std::pair<std::string, void*> keyValue : table) {
+            if (buffer.find(keyValue.first) == buffer.end()) {
+                buffer[keyValue.first] = keyValue.second;
+            }
         }
+    }
+
+    void* getAddress(const std::string& funcName) {
         return buffer[funcName];
     }
 
-    void addFuncPtr(std::string funcName, void* funcPtr) {
-        buffer[funcName] = funcPtr; // Rewrite if exists
+    void setAddress(const std::string& funcName, void* address) {
+        buffer.insert(std::make_pair(funcName, address));
     }
 
     void print() const {
+        std::cout << "--------------" << std::endl;
         std::cout << "vtable size: " << buffer.size() << std::endl;
         for (std::pair<std::string, void*> keyValue : buffer) {
             std::cout << keyValue.first << ": " << keyValue.second << std::endl;
         }
+        std::cout << "--------------" << std::endl;
     }
 
-    std::map<std::string, void*> buffer;
+    std::unordered_map<std::string, void*> buffer;
 };
 
 #define VIRTUAL_CLASS(Name) \
 class Name { \
 public: \
-	static std::shared_ptr<Vtable> vptr; \
+    static std::shared_ptr<Vtable> vptr;
 
-#define CLASS_END(Name) \
+#define END(Name) \
 }; \
 std::shared_ptr<Vtable> Name::vptr = std::make_shared<Vtable>();
 
-#define DECLARE_METHOD(ClassName, methodName) \
-void methodName() { \
-	std::cout << "Call " << #ClassName << "::" << #methodName << std::endl; \
-} \
+#define INIT_METHOD(className, methodName) \
+    className::vptr->setAddress(#methodName, (void(*)(className*)) &methodName);
 
-#define ADD_METHOD(object, methodName) \
-    object.vptr->addFuncPtr(#methodName, &methodName); \
-    object.vptr->print(); \
-
-#define VIRTUAL_CLASS_DERIVED(Name, BaseName) \
-class Name { \
-public: \
-	std::shared_ptr<Vtable> vptr; \
-
-#define CLASS_DERIVED_END(Name, BaseName) \
-	Name() \
-	{ \
-        vptr = std::make_shared<Vtable>((*BaseName::vptr).buffer); \
-	} \
-}; \
+#define DECLARE_METHOD(className, methodName) \
+void methodName(className* _this)
 
 #define VIRTUAL_CALL(object, methodName) \
 { \
-    typedef void func(); \
-    func* f = (func*)(object.vptr->getFuncPtr(#methodName)); \
-    f(); \
+    void* ptr = ((*object).vptr->getAddress(#methodName)); \
+    typedef void (*func)(); \
+    ((func) ptr)(); \
 }
+
+#define VIRTUAL_CLASS_DERIVED(Name, BaseName) \
+class Name : public BaseName { \
+public: \
+    static std::shared_ptr<Vtable> vptr;
+
+#define END_DERIVED(Name, BaseName) \
+    Name() { \
+        vptr->addNew((*BaseName::vptr).buffer); \
+    } \
+}; \
+std::shared_ptr<Vtable> Name::vptr = std::make_shared<Vtable>();
 
 
 VIRTUAL_CLASS(Base)
     int a;
-CLASS_END(Base)
-DECLARE_METHOD(Base, both)
-DECLARE_METHOD(Base, onlyBase)
+END(Base)
+
+DECLARE_METHOD(Base, both) {
+    std::cout << "Base::both" << std::endl;
+}
+
+DECLARE_METHOD(Base, onlyBase) {
+    std::cout << "Base::onlyBase" << std::endl;
+}
 
 VIRTUAL_CLASS_DERIVED(Derived, Base)
-    int b;
-CLASS_DERIVED_END(Derived, Base)
-DECLARE_METHOD( Derived, onlyDerived )
+END_DERIVED(Derived, Base)
 
-int main()
-{
-    Base b;
-    ADD_METHOD(b, both)
-    ADD_METHOD(b, onlyBase)
-    VIRTUAL_CALL( b, both )
+DECLARE_METHOD(Derived, both) {
+    std::cout << "Derived::both" << std::endl;
+}
 
-    Derived d;
-    ADD_METHOD(d, both)
-    ADD_METHOD(d, onlyDerived)
-    VIRTUAL_CALL(d, onlyDerived)
+DECLARE_METHOD(Derived, onlyDerived) {
+    std::cout << "Derived::onlyDerived" << std::endl;
+}
+
+int main() {
+
+    INIT_METHOD(Base, both)
+    INIT_METHOD(Base, onlyBase)
+    INIT_METHOD(Derived, both)
+    INIT_METHOD(Derived, onlyDerived)
+
+    Base base;
+    base.a = 0;
+
+    Derived derived;
+
+    base.vptr->print();
+    derived.vptr->print();
+
+    Base* reallyDerived = reinterpret_cast<Base*>(&derived);
+
+
+    VIRTUAL_CALL(&base, both)
+    VIRTUAL_CALL(reallyDerived, both)
+    VIRTUAL_CALL(reallyDerived, onlyBase)
+
     return 0;
 }
